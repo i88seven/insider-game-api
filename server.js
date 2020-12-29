ff012f52ecfc8a0563376e0ba79ea1867a596976
i88seven <i88.jelounjem@gmail.com>
2020-12-30 02:17:18 +0900

    Add server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+const PORT = process.env.PORT || 3001
+
+io.on('connection', socket => {
+  console.log(`socket_id: ${socket.id} is connected.`)
+
+  socket.on('join', roomId => {
+    socket.join(roomId)
+    console.log(`socket_id: ${socket.id} joined in room ${roomId}`)
+  })
+
+  socket.on('send-msg', (msg, roomId) => {
+    socket.to(roomId).emit('new-msg', msg)
+    console.log(`receive message: ${JSON.stringify(msg)}`)
+  })
+})
+
+server.listen(PORT)
